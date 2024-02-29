fe81db2bde373f44df9c82cb7925337d0cc029e9
milosKarakas <milos.karakas@codebluestudio.com>
2024-02-29 20:14:17 +0100

    Add encoder for upload

--- lib/delta_to_html.dart
+++ lib/delta_to_html.dart
@@ -1,10 +1,15 @@
 library delta_to_html;
 
 import 'src/encode.dart';
+import 'src/encode_for_upload.dart';
 
 class DeltaToHTML {
   /// Please Pass Delta JSON Value [ using .toJson() ] methods on QuillController  Documents like ( quillController.document.toDelta().toJson() ) to get HTML
   static encodeJson(List value) {
     return encoder(value);
+  }
+
+  static encodeJsonForUpload(List value) {
+    return encoderForUpload(value);
   }
 }
---
+++ lib/src/encode_for_upload.dart
@@ -0,0 +1,229 @@
+encoderForUpload(List delta) {
+  StringBuffer html = StringBuffer();
+  //! End Loop Implementation
+  delta.add({'insert': ' '});
+  List blockElements = [
+    'header',
+    'align',
+    'direction',
+    'list',
+    'blockquote',
+    'code-block',
+    'indent'
+  ];
+
+  int listIndex = -1;
+
+  for (var element in delta) {
+    listIndex++;
+
+    //! Embedded Implementation
+    if (element['insert'] is Map<String, dynamic>) {
+      //~ Image Implementation
+      if (element['insert'].containsKey('image')) {
+        String imageLink = element['insert']['image'].toString();
+        if (element.containsKey('attributes')) {
+          String style = element['attributes']['style']
+              .toString()
+              .replaceAll('mobile', '')
+              .replaceAll(':', "='")
+              .replaceAll(';', "'")
+              .toLowerCase();
+          html.write("<img src='$imageLink' $style>");
+        } else {
+          html.write("<img src='$imageLink'>");
+        }
+
+        //~ Video Implementation
+      } else if (element['insert'].containsKey('video')) {
+        String videoLink = element['insert']['video'].toString();
+        html.write("<embed type='video/webm' src='$videoLink'>");
+
+        //~ Line Implementation
+      } else if (element['insert'].containsKey('line')) {
+        html.write("<hr>");
+      }
+    } else {
+      //! Rich Text Implementation
+
+      //~ Normal Text Implementation
+      if (!element.containsKey('attributes') || (element['attributes'] is Map && element['attributes'].isEmpty)) {
+        html.write(element['insert'].toString());
+      } else {
+        String currentText = element['insert'].toString();
+        Map currentAttributeMap = element['attributes'] as Map;
+
+        //~ Inline Text Implementation
+        if (!blockElements.contains(currentAttributeMap.keys.first)) {
+          currentAttributeMap.forEach((key, value) {
+            switch (key.toString()) {
+              case "color":
+                currentText = "<span style='color:$value'>$currentText</span>";
+                break;
+              case "background":
+                currentText =
+                "<span style='background-color:$value'>$currentText</span>";
+                break;
+              case "font":
+                currentText =
+                "<span style='font-family:$value'>$currentText</span>";
+                break;
+
+              case "bold":
+                currentText = "<b>$currentText</b>";
+                break;
+              case "italic":
+                currentText = "<i>$currentText</i>";
+                break;
+              case "underline":
+                currentText = "<u>$currentText</u>";
+                break;
+              case "strike":
+                currentText = "<s>$currentText</s>";
+                break;
+
+              case "size":
+                switch (value) {
+                  case "small":
+                    currentText = "<small>$currentText</small>";
+                    break;
+                  case "large":
+                    currentText = "<big>$currentText</big>";
+                    break;
+                  case "huge":
+                    currentText =
+                    "<span style='font-size:150%'>$currentText</span>";
+                    break;
+                  default:
+                    currentText =
+                    "<span style='font-size:${value}px'>$currentText</span>";
+                    break;
+                }
+                break;
+
+              case "link":
+                currentText = "<a href='$value'>$currentText</a>";
+                break;
+
+              case "code":
+                currentText =
+                "<code style='color:#e1103a; background-color:#f1f1f1; padding: 0px 4px;'>$currentText</code>";
+                break;
+              default:
+            }
+          });
+          html.write(currentText);
+        } else {
+          //~ Block Text Implementation
+          String rawHtml = html.toString();
+
+          String blockString = '';
+          if (rawHtml.contains('\\Þ')) {
+            List dumpyStringList = rawHtml.split('\\Þ');
+            String initalPartThatShouldBeWritten = '';
+            bool shouldSeparateOnNewLine = currentAttributeMap.keys.contains('list');
+
+            if (shouldSeparateOnNewLine) {
+              List helperDumpyStringList = dumpyStringList.last.split('\n');
+              blockString = helperDumpyStringList.last;
+              helperDumpyStringList.removeLast();
+              initalPartThatShouldBeWritten = helperDumpyStringList.join('\n');
+            } else {
+              blockString = dumpyStringList.last;
+            }
+
+            dumpyStringList.removeLast();
+            String dumpyString = dumpyStringList.join();
+
+            dumpyString = dumpyString + initalPartThatShouldBeWritten;
+
+            html.clear();
+            html.write(dumpyString);
+          } else {
+            List dumpyStringList = rawHtml.split('\n');
+            blockString = dumpyStringList.last;
+            dumpyStringList.removeLast();
+            String dumpyString = dumpyStringList.join('\n');
+            html.clear();
+            html.write(dumpyString);
+          }
+
+          currentAttributeMap.forEach((key, value) {
+            switch (key.toString()) {
+              case "header":
+                currentText = "<h$value>$blockString</h$value>";
+                break;
+
+              case "align":
+                currentText = "<p style='text-align:$value'>$blockString</p>";
+                break;
+
+              case "direction":
+                currentText = "<p style='direction:$value'>$blockString</p>";
+                break;
+
+              case "code-block":
+                currentText =
+                "<pre><code style='color:#3F51B5; background-color:#f1f1f1; padding: 0px 4px;'>$blockString</code></pre>";
+                break;
+
+              case "blockquote":
+                currentText = "<blockquote>$blockString</blockquote>";
+                break;
+
+              case "list":
+                switch (value) {
+                  case "ordered":
+                    bool isStart = true, isEnd = true;
+
+                    try {
+                      isStart = delta[listIndex - 2]['attributes']['list'] !=
+                          "ordered";
+                      // ignore: empty_catches
+                    } catch (e) {}
+                    try {
+                      isEnd = delta[listIndex + 2]['attributes']['list'] !=
+                          "ordered";
+                      // ignore: empty_catches
+                    } catch (e) {}
+
+                    if (isStart && isEnd) {
+                      currentText = "<ol><li>$blockString</li></ol>";
+                    } else {
+                      if (isStart) {
+                        currentText = "<ol><li>$blockString</li>";
+                      } else if (isEnd) {
+                        currentText = "<li>$blockString</li></ol>";
+                      } else {
+                        currentText = "<li>$blockString</li>";
+                      }
+                    }
+
+                    break;
+                  case "bullet":
+                    currentText = "<ul><li>$blockString</li></ul>";
+                    break;
+                  case "checked":
+                    currentText =
+                    "<input type='checkbox' checked><label>$blockString</label><br>";
+                    break;
+                  case "unchecked":
+                    currentText =
+                    "<input type='checkbox' ><label>$blockString</label><br>";
+                    break;
+                }
+                break;
+
+              case "indent":
+                currentText = "${'&emsp;' * value} $blockString";
+                break;
+            }
+          });
+          html.write('$currentText\\Þ');
+        }
+      }
+    }
+  }
+
+  return html.toString().replaceAll('\\Þ', '');
+}
